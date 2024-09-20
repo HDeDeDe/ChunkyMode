9143c1bc36393fcbdf2bf91a152e8250a8e7ab5a
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-09-19 23:58:47 -0700

    automatically compress build into zip file compatible with r2modman

---
+++ CompressToZip/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Compression;
+
+string targetFile = "../ChunkyMode/bin/ChunkyMode.zip";
+
+FileInfo chunkyModeDLL;
+FileInfo chunkyDiffIcon = new FileInfo("../Resources/chunkydifficon");
+FileInfo changelog = new FileInfo("../CHANGELOG.md");
+FileInfo readme = new FileInfo("../README.md");
+FileInfo icon = new FileInfo("../Resources/icon.png");
+FileInfo manifest = new FileInfo("../Resources/manifest.json");
+
+#if DEBUG
+chunkyModeDLL = new FileInfo("../ChunkyMode/bin/Debug/netstandard2.1/ChunkyMode.dll");
+#endif
+
+#if RELEASE
+chunkyModeDLL = new FileInfo("../ChunkyMode/bin/Release/netstandard2.1/ChunkyMode.dll");
+#endif
+
+
+Console.WriteLine("Creating ChunkyMode.Zip");
+if (File.Exists(targetFile)) File.Delete(targetFile);
+
+ZipArchive archive = ZipFile.Open(targetFile, ZipArchiveMode.Create);
+
+archive.CreateEntryFromFile(chunkyDiffIcon.FullName, chunkyDiffIcon.Name, CompressionLevel.Optimal);
+archive.CreateEntryFromFile(changelog.FullName, changelog.Name, CompressionLevel.Optimal);
+archive.CreateEntryFromFile(readme.FullName, readme.Name, CompressionLevel.Optimal);
+archive.CreateEntryFromFile(chunkyModeDLL.FullName, chunkyModeDLL.Name, CompressionLevel.Optimal);
+archive.CreateEntryFromFile(icon.FullName, icon.Name, CompressionLevel.Optimal);
+archive.CreateEntryFromFile(manifest.FullName, manifest.Name, CompressionLevel.Optimal);
+
+archive.Dispose();
