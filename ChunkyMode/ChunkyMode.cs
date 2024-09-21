462930e21a2aca76c9888436fadd019e7d472092
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-09-21 14:01:33 -0700

    added loitering penalty

--- ChunkyMode/ChunkyMode.cs
+++ ChunkyMode/ChunkyMode.cs
@@ -16,17 +16,24 @@ namespace ChunkyMode
 
     public class ChunkyMode : BaseUnityPlugin
     {
+        //TODO: Buff Vicious Wounds and Ravenous Bite
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "HDeDeDe";
         public const string PluginName = "ChunkyMode";
-        public const string PluginVersion = "0.1.1";
+        public const string PluginVersion = "0.1.2";
 
         public AssetBundle ChunkyModeDifficultyModBundle;
         public static DifficultyDef ChunkyModeDifficultyDef;
         public static DifficultyIndex ChunkyModeDifficultyIndex;
         private const float rexHealOverride = 1.5f;
-        private static bool shouldRun = false;
+        private static bool shouldRun;
+        private static bool swarmsEnabled;
         private static int ogMonsterCap;
+
+        private static bool getFuckedLMAO;
+        private static bool teleporterExists;
+        private static float stagePunishTimer;
+        private static bool teleporterHit;
         
         public void Awake()
         {
@@ -70,11 +77,17 @@ namespace ChunkyMode
         private static void Run_onRunSetRuleBookGlobal(Run arg1, RuleBook arg2)
         {
             if (arg1.selectedDifficulty != ChunkyModeDifficultyIndex) return;
+            if (RunArtifactManager.instance.IsArtifactEnabled(RoR2Content.Artifacts.swarmsArtifactDef))
+                swarmsEnabled = true;
+            else swarmsEnabled = false;
             Run.ambientLevelCap = 9999;
         }
 
         private void Run_onRunStartGlobal(Run run) {
             shouldRun = false;
+            teleporterHit = false;
+            teleporterExists = false;
+            getFuckedLMAO = false;
             ogMonsterCap = TeamCatalog.GetTeamDef(TeamIndex.Monster).softCharacterLimit;
             
             if (run.selectedDifficulty != ChunkyModeDifficultyIndex) return;
@@ -94,6 +107,10 @@ namespace ChunkyMode
             On.RoR2.CombatDirector.Awake += CombatDirector_Awake;
             On.RoR2.HealthComponent.Heal += OnHeal;
             RecalculateStatsAPI.GetStatCoefficients += RecalculateStatsAPI_GetStatCoefficients;
+            
+            On.RoR2.Run.OnServerTeleporterPlaced += Run_OnServerTeleporterPlaced;
+            On.RoR2.Run.BeginStage += Run_BeginStage;
+            On.RoR2.TeleporterInteraction.IdleState.OnInteractionBegin += OnInteractTeleporter;
         }
 
         private void Run_onRunDestroyGlobal(Run run) {
@@ -113,6 +130,10 @@ namespace ChunkyMode
             On.RoR2.HealthComponent.Heal -= OnHeal;
             On.RoR2.CombatDirector.Awake -= CombatDirector_Awake;
             SceneDirector.onPrePopulateSceneServer -= SceneDirector_onPrePopulateSceneServer;
+            
+            On.RoR2.Run.OnServerTeleporterPlaced -= Run_OnServerTeleporterPlaced;
+            On.RoR2.Run.BeginStage -= Run_BeginStage;
+            On.RoR2.TeleporterInteraction.IdleState.OnInteractionBegin -= OnInteractTeleporter;
         }
         
         // This handles the -50% Ally Healing stat
@@ -150,6 +171,8 @@ namespace ChunkyMode
                     args.cooldownReductionAdd += 0.5f;
                     break;
             }
+            
+            if (getFuckedLMAO) args.healthMultAdd += 2.0f;
         }
 
         // This handles the +10% Enemy Spawn Rate stat and the hidden -10% Gold gain stat
@@ -164,6 +187,84 @@ namespace ChunkyMode
         private void SceneDirector_onPrePopulateSceneServer(SceneDirector self) {
             self.interactableCredit = (int)(self.interactableCredit * 1.2);
             Log.Info("Updated Credits: " + self.interactableCredit);
+        }
+
+        // Set up Loitering Punishment
+        private void Run_BeginStage(On.RoR2.Run.orig_BeginStage beginStage, Run self) {
+            teleporterHit = false;
+            teleporterExists = false;
+            getFuckedLMAO = false;
+            Log.Info("Stage begin! Waiting for Teleporter to be created.");
+            beginStage(self);
+        }
+        
+        // If a teleporter does not exist on the stage the loitering penalty should not be applied
+        private void Run_OnServerTeleporterPlaced(On.RoR2.Run.orig_OnServerTeleporterPlaced teleporterPlaced, Run self, SceneDirector sceneDirector, GameObject thing) {
+            teleporterExists = true;
+            stagePunishTimer = self.NetworkfixedTime + 300f;
+            Log.Info("Teleporter created! Timer set to " + stagePunishTimer);
+            teleporterPlaced(self, sceneDirector, thing);
+        }
+        
+        // Enforcing loitering penalty
+        private void FixedUpdate() {
+            if (!shouldRun) return;
+            if (swarmsEnabled || Run.instance.isGameOverServer) {
+#if DEBUG
+                ReportLoiterError("Swarms or Game Over");
+#endif
+                return;
+            }
+            if (teleporterHit) {
+#if DEBUG
+                ReportLoiterError("Teleporter hit");
+#endif
+                return;
+            }
+            if (!teleporterExists) {
+#if DEBUG
+                ReportLoiterError("Teleporter does not exist");
+#endif
+                return;
+            }
+            if (getFuckedLMAO){
+#if DEBUG
+                ReportLoiterError("Time's up");
+#endif
+                return;
+            }
+            if (stagePunishTimer >= Run.instance.NetworkfixedTime) {
+#if DEBUG
+                ReportLoiterError("Not time yet");
+#endif
+                return;
+            }
+            Log.Info("Time's up! Swarms has been enabled. StagePunishTimer " + stagePunishTimer);
+            getFuckedLMAO = true;
+            RunArtifactManager.instance.SetArtifactEnabled(RoR2Content.Artifacts.swarmsArtifactDef, true);
+        }
+        
+#if DEBUG
+        // Report why loitering hasn't been enabled every 5 seconds
+        private static float reportErrorTime;
+        private void ReportLoiterError(string err) {
+            if (reportErrorTime >= Run.instance.NetworkfixedTime) return;
+            Log.Info(err);
+            reportErrorTime = Run.instance.NetworkfixedTime + 5f;
+        }
+#endif
+        
+        // Disable loitering penalty when the teleporter is interacted with
+        private void OnInteractTeleporter(On.RoR2.TeleporterInteraction.IdleState.orig_OnInteractionBegin interact, EntityStates.BaseState teleporterState, Interactor interactor) {
+            if (swarmsEnabled) {
+                interact(teleporterState, interactor);
+                return;
+            }
+            if (RunArtifactManager.instance.IsArtifactEnabled(RoR2Content.Artifacts.swarmsArtifactDef)) 
+                RunArtifactManager.instance.SetArtifactEnabled(RoR2Content.Artifacts.swarmsArtifactDef, false);
+            getFuckedLMAO = false;
+            teleporterHit = true;
+            interact(teleporterState, interactor);
         }
 
         // This handles the -50% Ally Shield Recharge Rate stat
