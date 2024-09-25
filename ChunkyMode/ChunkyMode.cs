fd69dc36b80c14c77b295f1a5bacc7eeab9a61bc
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-09-25 16:18:48 -0700

    Moved Risk of Options support to a different file

--- ChunkyMode/ChunkyMode.cs
+++ ChunkyMode/ChunkyMode.cs
@@ -12,32 +12,6 @@ using RiskOfOptions.Options;
 
 namespace ChunkyMode
 {
-    public static class ChunkyModeOptions {
-        private static bool? _enabled;
-
-        public static bool enabled {
-            get {
-                if (_enabled == null) {
-                    _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions");
-                }
-                return (bool)_enabled;
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static void AddCheck(ConfigEntry<bool> option) {
-            ModSettingsManager.AddOption(new CheckBoxOption(option));
-        }
-        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static void SetSprite(Sprite sprite) {
-            ModSettingsManager.SetModIcon(sprite);
-        }
-        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static void SetDescription(string description) {
-            ModSettingsManager.SetModDescription(description);
-        }
-    }
-    
     [BepInDependency(DifficultyAPI.PluginGUID)]
     [BepInDependency(LanguageAPI.PluginGUID)]
     [BepInDependency(RecalculateStatsAPI.PluginGUID)]
