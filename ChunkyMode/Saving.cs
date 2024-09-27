85a106846c661a9df35c531aee94516a26562bea
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-09-27 15:52:14 -0700

    Debug logging for ProperSave support

--- ChunkyMode/Saving.cs
+++ ChunkyMode/Saving.cs
@@ -22,14 +22,26 @@ namespace ChunkyMode {
 		}
 		[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
 		private static void LoadFromSave(SaveFile save) {
+#if DEBUG
+			Log.Info("LoadFromSave called");			
+#endif
 			if (!Loading.IsLoading) return;
+#if DEBUG
+			Log.Info("Loading Chunky Mode save data");			
+#endif
 			RunInfo tempRun = save.GetModdedData<RunInfo>("CHUNKYMODE_RunInfo");
 			if (tempRun == null) return;
+#if DEBUG
+			Log.Info("Applying Chunky Mode Save");			
+#endif
 			RunInfo.Instance = tempRun;
 			RunInfo.preSet = true;
 		}
 		[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
 		private static void SaveRunInfo(Dictionary<string, object> save) {
+#if DEBUG
+			Log.Info("Saving Chunky Mode data");			
+#endif
 			save.Add("CHUNKYMODE_RunInfo",RunInfo.Instance);
 		}
 	}
