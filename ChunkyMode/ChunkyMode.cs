0f7abd278c895215374aa6341c04a7d4692a7220
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-10-10 14:24:59 -0700

    increment version number

--- ChunkyMode/ChunkyMode.cs
+++ ChunkyMode/ChunkyMode.cs
@@ -24,7 +24,7 @@ namespace HDeMods
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "HDeDeDe";
         public const string PluginName = "ChunkyMode";
-        public const string PluginVersion = "0.2.0";
+        public const string PluginVersion = "0.2.1";
 
         // Difficulty related variables
         public AssetBundle ChunkyModeDifficultyModBundle;
