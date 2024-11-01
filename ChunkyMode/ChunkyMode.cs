b2b46151f25ca00cd15a1e3d0ef8c836fc938879
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-10-31 21:05:58 -0700

    update version number

--- ChunkyMode/ChunkyMode.cs
+++ ChunkyMode/ChunkyMode.cs
@@ -35,7 +35,7 @@ namespace HDeMods
         public const string PluginGUID = "com." + PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "HDeDeDe";
         public const string PluginName = "ChunkyMode";
-        public const string PluginVersion = "0.3.0";
+        public const string PluginVersion = "0.3.1";
         public static ChunkyMode instance;
         public static ConfigFile experiments;
 
