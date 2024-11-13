e506b9a48723deba2fe8fd76dcbaafdb7cb6c25e
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-11-12 22:50:00 -0800

    update healthcomponent api

--- CompressToZip/Settings.cs
+++ CompressToZip/Settings.cs
@@ -36,7 +36,7 @@ internal static class Settings {
                                                "\t\t\"RiskofThunder-R2API_RecalculateStats-1.4.0\",\n" +
                                                "\t\t\"RiskofThunder-R2API_Networking-1.0.2\",\n" +
                                                "\t\t\"RiskofThunder-R2API_Director-2.3.1\",\n" +
-                                               "\t\t\"HDeDeDe-HealthComponentAPI-0.2.0\",\n" +
+                                               "\t\t\"HDeDeDe-HealthComponentAPI-1.1.0\",\n" +
                                                "\t\t\"HDeDeDe-InterlopingArtifact-0.1.1\",\n" +
                                                "\t\t\"KingEnderBrine-ScrollableLobbyUI-1.9.1\"\n" +
                                                "\t]";
