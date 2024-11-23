da7cb60529614588670e84c7fd49dfaf0e8ac4fb
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-11-23 14:15:28 -0800

    remove multiplayer guard

--- ChunkyMode/SadomasochismWish.cs
+++ ChunkyMode/SadomasochismWish.cs
@@ -105,16 +105,6 @@ namespace HDeMods {
                     []), CommonLanguageTokens.ok);
         }
 
-        internal static void DisconnectIfBrimstoneOff(NetworkConnection conn) {
-            if (NetworkServer.active) return;
-            if (HurricaneOptionalMods.InfernoDownpour.DiffsEnabled()) return;
-            // Return if rule is present in lobby
-            RoR2.Networking.NetworkManagerSystem.singleton.Disconnect();
-            DialogBoxManager.DialogBox( new SimpleDialogBox.TokenParamsPair("DISCONNECTED", []), 
-                new SimpleDialogBox.TokenParamsPair("SADOMASOCHISMWISH_BRIMSTONE_MISSING_MSG", []), 
-                CommonLanguageTokens.ok);
-        }
-
         private static void AppendDiff() {
             Downpour.DownpourPlugin.DownpourList.Add(diffDef);
             Downpour.DownpourPlugin.BrimstoneList.Add(diffDef);
